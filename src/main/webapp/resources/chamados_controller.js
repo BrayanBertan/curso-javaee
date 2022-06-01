01945cf59ad9b55a3bb740d58213998680dbcc80
Brayan <brayanbertan@gmail.com>
2022-06-01 15:51:52 -0300

    Aula 8

---
+++ src/main/webapp/resources/chamados_controller.js
@@ -0,0 +1,12 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+var app = angular.module("chamadosApp",[]);
+
+app.controller("chamadosController", function ($scope){
+    $scope.teste = "Teste app";
+});
+
+
